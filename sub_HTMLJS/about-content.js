094112ea73011d1b784d0395b55793c05342df4a
Max Hernandez Guzman <maxthg@gmail.com>
2024-06-02 21:21:19 -0400

    transfer files from old website, update name, update color scheme

---
+++ sub_HTMLJS/about-content.js
@@ -0,0 +1,40 @@
+var randvar1 = '<div class="about-content">'+
+'    <div class="about-intro">'+
+'           <div class="intro-img">'+
+'               <img src="images/about-intro-img-crop.jpg" alt="me!">'+
+'           </div>'+
+'        <div class="intro-msg">'+
+'            <div class="intro-title">'+
+'                <h2>About Arty (she/they)</h2>'+
+'            </div>'+
+'            <div class="intro-main">'+
+'                <p> I am a fifth year Mechanical Engineering student at the Georgia Institute of Technology, currently' +
+'                    interning at NASA JPL for the summer in the Entry, Descent, & Landing and Formulation group (352B).</p>' +
+'                <p> Currently seeking a graduate program in Mechanical/Space Engineering starting in Fall 2025. Open to Masters or Ph.D.</p>'+
+'                <p> I believe that through perseverance, innovation, and cooperation, we can'+
+'                    accomplish great things. If you need a compassionate, perseverant, and future-focused leader with a diverse technical background, you\'ve found the right person. </p>'+
+'                <p> My hobbies include rock climbing, making music, playing board games, and going on fun adventures with friends! </p>'+
+'            </div>'+
+'        </div>'+
+'        <div class="about-links">'+
+'            <div></div>'+
+'            <div class="about-resume">'+
+'                <div class="link-box">'+
+'                    <a href="resume/resume_artyhg.pdf" target="_blank" download'+
+'                        title="Click to download a copy of my resume">'+
+'                        <h4>Resume</h4>'+
+'                    </a>'+
+'                </div>'+
+'            </div>'+
+'            <div class="about-projects">'+
+'                <div class="link-box">'+
+'                    <a href="portfolio.html" title="Link to my Portfolio page">'+
+'                        <h4>Projects</h4>'+
+'                    </a>'+
+'                </div>'+
+'            </div>'+
+'            <div></div>'+
+'        </div>'+
+'    </div>'+
+'</div>';
+document.write(randvar1);
---
+++ sub_HTMLJS/footer.js
@@ -0,0 +1,23 @@
+var randvar4 = '<div class="footer">'+
+'    <div class="foot-container">'+
+'        <div class="footer-links">'+
+'            <a href="index.html" class="footer-link">Home |</a>  '+
+'            <a href="portfolio.html" class="footer-link">Portfolio |</a> '+
+'            <a href="about.html" class="footer-link">About |</a> '+
+'            <a href="contact.html" class="footer-link">Contact</a>'+
+'        </div>'+
+'        <div class="footer-text">'+
+'            <p class="copyright">Developed by Artemis Hernandez Guzman &#8212 V 1.3.1</p>'+
+'            <p class="perfectionist"> I strive to make my work the best it can be, so if you find any bugs, have any suggestions, or just want to say hi, feel free to <a href="contact.html">contact me</a>!</p>'+
+'        </div>'+
+'        <div class="footer-socials">'+
+'            <div class=" social-box social-box-1">'+
+'                <a href="https://linkedin.com/in/artemishg" target="_blank"><img src="images/linkedinIcon.png" alt="LinkedIn Profile Icon" class="social-icon social1"></a>'+
+'            </div>'+
+'            <div class="social-box social-box-2">'+
+'                <a href="mailto:artemis.hg@proton.me" target="_blank"><img src="images/emailIcon.png" alt="Email Icon" class="social-icon social2"></a>'+
+'            </div>'+
+'        </div>'+
+'    </div>'+
+'</div>';
+document.write(randvar4);
